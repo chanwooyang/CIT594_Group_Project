fc8af43f7cd5c64e6aeb61b484ca5832472df96b
Peter Brice <pbrice@seas.upenn.edu>
2019-12-09 21:58:00 +0800

    Logging Updates

--- src/edu/upenn/cit594/datamanagement/CSVParkingViolationReader.java
+++ src/edu/upenn/cit594/datamanagement/CSVParkingViolationReader.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 
 import edu.upenn.cit594.data.*;
+import edu.upenn.cit594.logging.Logger;
 
 public class CSVParkingViolationReader extends ParkingViolationReader {
 
@@ -19,6 +20,9 @@ public class CSVParkingViolationReader extends ParkingViolationReader {
 
 		try {
 			FileReader csv = new FileReader(fileName);
+			// whenever an input file is opened for reading, the program should "log it."
+			Logger lg = Logger.getInstance();
+			lg.log(lg.getTime() + " "  + fileName);
 			@SuppressWarnings("resource")
 			Scanner sc = new Scanner(csv);
 
